a5dbc3b47ddea7ae05e12e4a69a17377d9b85518
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-10-23 15:40:52 +0100

    Basic layout of homepage

--- JobApplicationTracker/ContentView.swift
+++ JobApplicationTracker/ContentView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            VStack {
+                ForEach(0..<2) { _ in
+                    HStack {
+                        ForEach(0..<2) { _ in
+                            Button {
+                                print("Button was pressed")
+                            } label: {
+                                Text("Click Me")
+                                    .frame(width: 150, height: 150)
+                                    .background(.red)
+                                    .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+                                    .padding(10)
+                            }
+                        }
+                    }
+                }
+                
+            }
+            .padding()
+            Spacer()
+            Section("My applications") {
+                List {
+                    ForEach(0..<15) { _ in
+                        Text("Zip a dee doo dah")
+                    }
+                }
+            }
+    }
+
     }
 }
 
