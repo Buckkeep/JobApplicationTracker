ebca8337cc86b9a831a30bc21234d2931b45c1a3
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-10-23 15:16:16 +0100

    Initial Commit

---
+++ JobApplicationTracker/JobApplicationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobApplicationTrackerApp.swift
+//  JobApplicationTracker
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JobApplicationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
