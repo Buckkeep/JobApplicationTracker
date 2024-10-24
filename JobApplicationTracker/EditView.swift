10d50bb0b4d10ffad7944275d50a2bcfd3c6f1ba
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-10-24 11:52:04 +0100

    EditView page - changed keyboard types for URL and numeric. Created new section for notes and added vertical axis for longer notes.

--- JobApplicationTracker/EditView.swift
+++ JobApplicationTracker/EditView.swift
@@ -26,11 +26,15 @@ struct EditView: View {
             TextField("Job Title", text: $viewModel.jobTitle)
             TextField("Location", text:$viewModel.location)
             TextField("Salary", value: $viewModel.salary, format: .currency(code: Locale.current.currency?.identifier ?? "GBP"))
+                .keyboardType(.numberPad)
             TextField("Listing URL", text: $viewModel.listingURL)
+                .keyboardType(.URL)
+                .textContentType(.URL)
             DatePicker("Date Applied", selection: $viewModel.dateApplied, displayedComponents: .date)
             Toggle("Followed up?", isOn: $viewModel.followedUp)
-            TextField("Notes", text: $viewModel.notes)
-            
+            Section("Notes") {
+                TextField("Notes", text: $viewModel.notes, axis: .vertical)
+            }
         }
     }
 }
